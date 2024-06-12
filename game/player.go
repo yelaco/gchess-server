0a072465e6843bd87681e5e9eb8913416794ae39
Minh Quang Bui <minhquangbui053@gmail.com>
2024-06-12 17:18:47 +0700

    Add player type

---
+++ game/player.go
@@ -0,0 +1,22 @@
+package game
+
+import "github.com/minhquang053/robinhood-chess/pkg/utils"
+
+type player struct {
+	playerId    string
+	isWhiteSide bool
+}
+
+func initPlayers() *[2]player {
+	players := [2]player{
+		{
+			playerId:    utils.GenerateUUID(),
+			isWhiteSide: true,
+		},
+		{
+			playerId:    utils.GenerateUUID(),
+			isWhiteSide: false,
+		},
+	}
+	return &players
+}
