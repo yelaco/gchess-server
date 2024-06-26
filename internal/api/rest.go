e834e76cc986e55425bf0e141a0cf3709697c709
Minh Quang Bui <minhquangbui053@gmail.com>
2024-06-27 01:05:07 +0700

    Change imported package name due to project rename

--- internal/api/rest.go
+++ internal/api/rest.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/yelaco/robinhood-chess/pkg/config"
-	"github.com/yelaco/robinhood-chess/pkg/logging"
+	"github.com/yelaco/go-chess-server/pkg/config"
+	"github.com/yelaco/go-chess-server/pkg/logging"
 	"go.uber.org/zap"
 )
 
