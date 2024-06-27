dc7597b9e351314264ec9065bb2fc97e333167ef
Minh Quang Bui <minhquangbui053@gmail.com>
2024-06-27 16:58:13 +0700

    Add tview for command line interace

--- cmd/game-client/client.go
+++ cmd/game-client/client.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
@@ -11,10 +15,16 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/rivo/tview"
 	"github.com/yelaco/go-chess-server/internal/corenet"
 	"github.com/yelaco/go-chess-server/internal/session"
 	"github.com/yelaco/go-chess-server/pkg/config"
 )
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 type matchResponse struct {
 	Type        string              `json:"type"`
@@ -23,38 +33,88 @@ type matchResponse struct {
 	PlayerState session.PlayerState `json:"player_state"`
 }
 
+var currentUser *User
+var playerID string
+var loginForm *tview.Form
+var registerForm *tview.Form
+
 func main() {
+	app := tview.NewApplication()
+
+	loginForm = tview.NewForm().
+		AddInputField("Username", "", 20, nil, nil).
+		AddPasswordField("Password", "", 20, '*', nil).
+		AddButton("Login", func() {
+			username := loginForm.GetFormItemByLabel("Username").(*tview.InputField).GetText()
+			password := loginForm.GetFormItemByLabel("Password").(*tview.InputField).GetText()
+			user := User{Username: username, Password: password}
+			token, err := login(user)
+			if err != nil {
+				log.Printf("Login failed: %v", err)
+				return
+			}
+			currentUser = &user
+			playerID = token
+			app.SetRoot(postLoginMenu(app), true).Run()
+		}).
+		AddButton("Back", func() {
+			app.SetRoot(mainMenu(app), true).Run()
+		})
+
+	registerForm = tview.NewForm().
+		AddInputField("Username", "", 20, nil, nil).
+		AddPasswordField("Password", "", 20, '*', nil).
+		AddButton("Register", func() {
+			username := registerForm.GetFormItemByLabel("Username").(*tview.InputField).GetText()
+			password := registerForm.GetFormItemByLabel("Password").(*tview.InputField).GetText()
+			user := User{Username: username, Password: password}
+			err := register(user)
+			if err != nil {
+				log.Printf("Register failed: %v", err)
+				return
+			}
+			fmt.Println("Register successful!")
+			app.SetRoot(mainMenu(app), true).Run()
+		}).
+		AddButton("Back", func() {
+			app.SetRoot(mainMenu(app), true).Run()
+		})
+
+	app.SetRoot(mainMenu(app), true).Run()
+}
+
+func joinMatch() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	u := url.URL{Scheme: "ws", Host: "localhost:" + config.Port, Path: "/ws"}
-	log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
-
-	playerID := "11225"
+	log.Println("Connected to game server")
 
 	done := make(chan struct{})
 
 	go func() {
 		defer close(done)
-		c.WriteJSON(corenet.Message{
+		if err := c.WriteJSON(corenet.Message{
 			Action: "matching",
 			Data: map[string]interface{}{
 				"player_id": playerID,
 			},
-		})
+		}); err != nil {
+			log.Fatal("ws write", err)
+		}
+
+		log.Println("Attemp matchmaking...")
 
 		var resp matchResponse
 		if err := c.ReadJSON(&resp); err != nil {
 			log.Fatal("ws match:", err)
 			return
-		} else {
-			fmt.Println(resp.Type)
 		}
 
 		if resp.Type != "matched" {
@@ -73,7 +133,7 @@ func main() {
 				state = sessionResp.GameState
 			}
 			clearScreen()
-			printBoard(state.Board)
+			printBoard(state.Board, resp.PlayerState.IsWhiteSide)
 			if state.Status != "ACTIVE" {
 				fmt.Println(state.Status)
 				return
@@ -129,24 +189,184 @@ func main() {
 	}
 }
 
-func printBoard(board [8][8]string) {
-	fmt.Println("  +-----------------+")
+func mainMenu(app *tview.Application) *tview.Flex {
+	headerBox := tview.NewBox().
+		SetBorder(true).
+		SetTitle("Go Chess Server").
+		SetTitleAlign(tview.AlignLeft)
 
-	for i := 7; i >= 0; i-- {
-		fmt.Printf("%d | ", i+1)
-		for j := 0; j < 8; j++ {
-			box := board[j][i]
-			if box != "" {
-				fmt.Print(box + " ")
-			} else {
-				fmt.Print(". ")
-			}
+	headerText := tview.NewTextView().
+		SetDynamicColors(true).
+		SetTextAlign(tview.AlignLeft)
+
+	updateHeader := func() {
+		if currentUser == nil {
+			headerText.SetText("Please login to the server")
+		} else {
+			headerText.SetText(fmt.Sprintf("User: %s", currentUser.Username))
 		}
-		fmt.Println("|")
 	}
 
+	updateHeader()
+
+	menu := tview.NewList().
+		AddItem("Login", "Login to your account", '1', func() {
+			app.SetRoot(loginForm, true).Run()
+		}).
+		AddItem("Register", "Register a new account", '2', func() {
+			app.SetRoot(registerForm, true).Run()
+		}).
+		AddItem("Quit", "Exit the application", '3', func() {
+			app.Stop()
+			os.Exit(0)
+		})
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(headerBox, 3, 1, false).
+		AddItem(headerText, 1, 1, false).
+		AddItem(menu, 0, 1, true)
+
+	return flex
+}
+
+func postLoginMenu(app *tview.Application) *tview.Flex {
+	headerBox := tview.NewBox().
+		SetBorder(true).
+		SetTitle("Go Chess Server").
+		SetTitleAlign(tview.AlignLeft)
+
+	headerText := tview.NewTextView().
+		SetDynamicColors(true).
+		SetTextAlign(tview.AlignLeft)
+
+	updateHeader := func() {
+		if currentUser == nil {
+			headerText.SetText("Please login to the server")
+		} else {
+			headerText.SetText(fmt.Sprintf("User: %s", currentUser.Username))
+		}
+	}
+
+	updateHeader()
+
+	menu := tview.NewList().
+		AddItem("Join Match", "Join a new match", '1', func() {
+			app.Stop()
+			joinMatch()
+		}).
+		AddItem("Other Option", "Perform another action", '2', func() {
+			fmt.Println("Other Option selected!")
+		}).
+		AddItem("Logout", "Logout from your account", '3', func() {
+			currentUser = nil
+			app.SetRoot(mainMenu(app), true).Run()
+		})
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(headerBox, 3, 1, false).
+		AddItem(headerText, 1, 1, false).
+		AddItem(menu, 0, 1, true)
+
+	return flex
+}
+
+func register(user User) error {
+	url := "http://localhost:7202/register"
+	userJSON, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("error marshalling user: %w", err)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(userJSON))
+	if err != nil {
+		return fmt.Errorf("error making POST request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("register failed: %s", body)
+	}
+
+	fmt.Println("User registered successfully")
+	return nil
+}
+
+func login(user User) (string, error) {
+	// url := "http://localhost:7202/login"
+	// userJSON, err := json.Marshal(user)
+	// if err != nil {
+	// 	return "", fmt.Errorf("error marshalling user: %w", err)
+	// }
+
+	// resp, err := http.Post(url, "application/json", bytes.NewBuffer(userJSON))
+	// if err != nil {
+	// 	return "", fmt.Errorf("error making POST request: %w", err)
+	// }
+	// defer resp.Body.Close()
+
+	// if resp.StatusCode != http.StatusOK {
+	// 	body, _ := io.ReadAll(resp.Body)
+	// 	return "", fmt.Errorf("login failed: %s", body)
+	// }
+
+	// var result map[string]interface{}
+	// err = json.NewDecoder(resp.Body).Decode(&result)
+	// if err != nil {
+	// 	return "", fmt.Errorf("error decoding response: %w", err)
+	// }
+
+	// // Assuming the token is part of the response
+	// token, ok := result["token"].(string)
+	// if !ok {
+	// 	return "", fmt.Errorf("token not found in response")
+	// }
+
+	// fmt.Println("User logged in successfully")
+	// return token, nil
+
+	return "11234", nil
+}
+
+func printBoard(board [8][8]string, isWhiteSide bool) {
 	fmt.Println("  +-----------------+")
-	fmt.Println("    a b c d e f g h")
+
+	if isWhiteSide {
+		for i := 7; i >= 0; i-- {
+			fmt.Printf("%d | ", i+1)
+			for j := 0; j < 8; j++ {
+				box := board[j][i]
+				if box != "" {
+					fmt.Print(box + " ")
+				} else {
+					fmt.Print(". ")
+				}
+			}
+			fmt.Println("|")
+		}
+
+		fmt.Println("  +-----------------+")
+		fmt.Println("    a b c d e f g h")
+	} else {
+		for i := 0; i < 8; i++ {
+			fmt.Printf("%d | ", i+1)
+			for j := 7; j >= 0; j-- {
+				box := board[j][i]
+				if box != "" {
+					fmt.Print(box + " ")
+				} else {
+					fmt.Print(". ")
+				}
+			}
+			fmt.Println("|")
+		}
+
+		fmt.Println("  +-----------------+")
+		fmt.Println("    h g f e d c b a ")
+	}
+
 	fmt.Println()
 }
 
--- go.mod
+++ go.mod
@@ -8,17 +8,21 @@ require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
-	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/tview v0.0.0-20240625185742-b0a7293b8130 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -34,6 +38,7 @@ require (
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
