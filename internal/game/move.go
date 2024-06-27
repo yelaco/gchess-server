c43e109b1a5fb597ec3d13e85145a2686a65e102
Minh Quang Bui <minhquangbui053@gmail.com>
2024-06-27 12:43:09 +0700

    Add parse move for move string from websocket messages

--- internal/game/move.go
+++ internal/game/move.go
@@ -1,5 +1,10 @@
 package game
 
+import (
+	"errors"
+	"strings"
+)
+
 type MoveStatus string
 
 type move struct {
@@ -22,6 +27,42 @@ func mapChessPosToCoord(pos string) (x int, y int) {
 	x = int(pos[0] - 'a')
 	y = int(pos[1] - '1')
 	return
+}
+
+func ParseMove(move string) ([]string, error) {
+	move = strings.TrimSpace(move)
+	if len(move) != 5 {
+		return []string{}, errors.New("couldn't parse move")
+	}
+
+	if move[2] != '-' {
+		return []string{}, errors.New("couldn't parse move")
+	}
+
+	if move[0] >= 'a' && move[0] <= 'h' &&
+		move[1] >= '1' && move[1] <= '8' &&
+		move[3] >= 'a' && move[3] <= 'h' &&
+		move[4] >= '1' && move[4] <= '8' {
+		pos := strings.Split(move, "-")
+		return pos, nil
+	} else {
+		return []string{}, errors.New("couldn't parse move")
+	}
+}
+func IsValidMove(move string) bool {
+	move = strings.TrimSpace(move)
+	if len(move) != 5 {
+		return false
+	}
+
+	if move[2] != '-' {
+		return false
+	}
+
+	return move[0] >= 'a' && move[0] <= 'h' &&
+		move[1] >= '1' && move[1] <= '8' &&
+		move[3] >= 'a' && move[3] <= 'h' &&
+		move[4] >= '1' && move[3] <= '8'
 }
 
 func (g *Game) GetLastMove() *move {
